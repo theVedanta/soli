a186ad732fc39638452c867a8da03d752ede67b3
theVedanta <71117219+theVedanta@users.noreply.github.com>
2020-12-19 22:50:18 +0530

    second

--- js/index.js
+++ js/index.js
@@ -1,10 +1,4 @@
 
-let gr = document.querySelector(".units");
-
-let line = document.querySelector(".grLine");
-line.style.height = `${gr.offsetHeight + 40}px`;
-
-
 let num = document.querySelector(".num");
 let counter = 0;
 let pause = document.querySelector(".pause");
