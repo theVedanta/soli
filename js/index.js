d0c3ac6ab741d4df7cfec15f735e9aecb503678c
theVedanta <71117219+theVedanta@users.noreply.github.com>
2020-12-20 10:40:16 +0530

    third

--- js/index.js
+++ js/index.js
@@ -11,3 +11,19 @@ setInterval(() => {
         num.textContent = `${counter}%`
     };
 }, 80);
+
+
+let links = document.querySelectorAll(".alLink");
+let inp = document.querySelector("#inp");
+let inpVal = inp.value;
+inp.addEventListener("keyup", () => {
+    inpVal = inp.value;
+});
+for (let link of links) {
+    link.addEventListener("mouseover", () => {
+        inp.value = link.getAttribute('data-val');;
+    });
+    link.addEventListener("mouseout", () => {
+        inp.value = inpVal;
+    });
+};
